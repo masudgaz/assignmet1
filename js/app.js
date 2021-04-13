cbfd234e4fc33dd107320dc9ca2bd22f9fdb363b
masudgaz <74758638+masudgaz@users.noreply.github.com>
2021-04-13 11:59:36 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $('.button').click(function(){
+
+        $(this).addClass('active').siblings().removeClass('active');
+
+        var filter = $(this).attr('data-filter');
+
+        if(filter == 'all') {
+            $('.image').show(400);
+        } else {
+            $('.image').not('.'+filter).hide(200);
+            $('.image').filter('.'+filter).show(400);
+        }
+
+    });
+
+    $('.gallery').magnificPopup({
+
+        delegate: 'a',
+        type:'image',
+        gallery: {
+            enabled: true
+        }
+
+    })
+
+});
